bff34d09f139a56ed069b16e2a5b19dd67be17a4
David-Whiteford <c00204740@itcarlow.ie>
2019-12-10 12:05:43 +0000

    Cleaned folder

--- BrowBoyBeats/www/browboybeats/platforms/android/app/src/main/assets/www/js/renderer.js
+++
@@ -1,9 +0,0 @@
-
-function Render_Screen(canvas) 
-{
-    var context = canvas.getContext("2d")
-   
-    // Clears the game canvas screen
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    
-}
