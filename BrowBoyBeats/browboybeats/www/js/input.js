7c29f52bd146daac97c11d759e0916fdb1d11579
PaulJame5 <c00231934@itcarlow.ie>
2019-12-12 11:00:02 +0000

    removed redundancy code

--- BrowBoyBeats/browboybeats/www/js/input.js
+++ BrowBoyBeats/browboybeats/www/js/input.js
@@ -108,7 +108,7 @@ class Input
         this.pressedRight = false;
         this.pressedUp = false;
     } // on touch end end
-
+    /// ================= END TOUCH INPUT ==========================///
     
     directionalInputCheck()
     {
@@ -209,111 +209,4 @@ class Input
    
     
 }
-Player.prototype.onTouchStart = function(e)
-{
-    this.held =true;
-    this.touches = e.touches;
-/**
-  get the start of touch position x and y create two variables and store the start x and y
-*/
-    this.startX = e.touches[0].clientX;
-    this.startY = e.touches[0].clientY;
-    this.startForX = this.startX;
-    this.startForY = this.startY;
-    this.time1 = new Date();
-    this.tappedX = this.startForX;
-    this.tappedY =this.startForY;
-    this.tapped = true;
-
-    //console.log("Start of swipe " +this.startX +  "," + this.startY);
-}
-
-/**
-* 
-* Function to get whether there is a move across the screen, also draws a line along the movement and get the end x and y
-* also update start to be at the end as  finger is moved 
-* @param {Object} e event hanlder
-*     
-*/
-Player.prototype.onTouchMove = function(e)
-{
-    this.changedTouches = e.changedTouches;
-    this.endX = e.changedTouches[0].clientX;
-    this.endY = e.changedTouches[0].clientY;
-/**
- * sets up the line
- */
-
-    this.startX = this.endX;
-    this.startY = this.endY;
-
-}
-
-
-Player.prototype.getName = function()
-{
-    return this.name;
-}
-
-/**
-* 
-* Function to get the end of a touch and the time when touch ends
-* also gets lenght of swipe to determine if its long enough to swipe.
-* @param {Object} e event hanlder
-*    
-*/
-Player.prototype.onTouchEnd = function(e)
-{
-
-    
-    this.tapped = false;
-    var x;
-    var y;
-/**
- * get the time and timplaspsed
- */
-    this.time2 = new Date();
-    var timeLapsed =this.time2 -this.time1;
-/** 
-*get and set the end x and y 
-*get value for x squared and multiply
-*/
-    this.endSwipeX = this.endX;
-    this.endSwipeY = this.endY;
-    x = this.endSwipeX - this.startForX;
-    x = x * x;
-
-/** 
-*get value for y squared and multiply
-* get the lenght of swipe usin the square root of x +y
-*/
-    y = this.endSwipeY - this.startForY;
-    y = y * y;
-
-    this.LeghtOfSwipe = Math.sqrt((x+y));
-/**
- * check if the time is greater tham 360 and thhe swipe is greater than 240
- */
-    if(timeLapsed >= 360 && this.LeghtOfSwipe >= 240)
-    {
-        this.tapped = false;
-        this.swipped = false;
-       
-        console.log("A swipe was done in game");
-    }
-
-    this.held = false;
-
-    if(timeLapsed >= 1)
-    {
-    
-        this.tapped = true;
-    }
-   
-
-}
-
-
-
-
 /* End Input Detection for Player */
